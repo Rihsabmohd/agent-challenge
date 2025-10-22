940e8148dec6d20bc6367065046ec17d86eb15d6
Rihsabmohd <rihsabmohd@gmail.com>
2025-10-23 00:22:58 +0100

    Update src/mastra/mcp/index.ts

--- src/mastra/mcp/index.ts
+++ src/mastra/mcp/index.ts
@@ -1,12 +1,12 @@
 import { MCPServer } from "@mastra/mcp"
-import { weatherTool } from "../tools";
-import { weatherAgent } from "../agents";
+import { searchPapersTool, getPaperDetailsTool, searchByAuthorTool} from "../tools";
+import { researchAgent } from "../agents";
 
 export const server = new MCPServer({
   name: "My Custom Server",
   version: "1.0.0",
-  tools: { weatherTool },
-  agents: { weatherAgent }, // this agent will become tool "ask_weatherAgent"
+  tools: { searchPapersTool, getPaperDetailsTool, searchByAuthorTool},
+  agents: { researchAgent }, // this agent will become tool "ask_weatherAgent"
   // workflows: {
   // dataProcessingWorkflow, // this workflow will become tool "run_dataProcessingWorkflow"
   // }
