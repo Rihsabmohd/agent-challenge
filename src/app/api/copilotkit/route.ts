6a65cc27c938e8ddd8e455fb3519bbe27a15b55d
Rihsabmohd <rihsabmohd@gmail.com>
2025-10-23 00:22:57 +0100

    Update src/app/api/copilotkit/route.ts

--- src/app/api/copilotkit/route.ts
+++ src/app/api/copilotkit/route.ts
@@ -1,15 +1,20 @@
 import {
   CopilotRuntime,
   ExperimentalEmptyAdapter,
+  OpenAIAdapter,
   copilotRuntimeNextJSAppRouterEndpoint,
 } from "@copilotkit/runtime";
 import { MastraAgent } from "@ag-ui/mastra"
 import { NextRequest } from "next/server";
 import { mastra } from "@/mastra";
+import OpenAI from '@ai-sdk/openai';
  
+const OLLAMA_API_URL = process.env.OLLAMA_API_URL || 'http://127.0.0.1:11434/api';
+const MODEL_NAME = process.env.MODEL_NAME_AT_ENDPOINT || 'qwen3:8b';
+
+
 // 1. You can use any service adapter here for multi-agent support.
 const serviceAdapter = new ExperimentalEmptyAdapter();
-
 // 2. Build a Next.js API route that handles the CopilotKit runtime requests.
 export const POST = async (req: NextRequest) => {
 
